5c5727b0118ccc6cda5be85c6341bf2a7095a1ac
kantapong-seasky <kuroka151725@gmail.com>
2025-05-14 20:21:51 +0700

    001 test

--- backend/route/subject.js
+++ backend/route/subject.js
@@ -1,40 +1,9 @@
 const express = require('express');
-const { subscribe } = require('./subscribe');
 const router = express.Router();
-/*
+const subject = require('../data/contact_subject.json');
 
-*/
-
-
-router.post('/', (req,res)=>{ 
-   const {email}= req.body
-   const subcribe = { subscribeAt : new Date(), email};
-
-   const filePath = Path2D.join(__dirname,'. .','data','subscibe.json');
-
-   console.log('Content from submited',{ email });
-
-   let subcribes = [];
-
-   if(FileSystem.existsSync(filePath)){
-    // file is here
-    const filedata = fs.readFileSync(filePath, 'utf-8');
-    subcribes = JSON.parse(filedata);
-    subcribes.push(subcribe);
-    fs.writeFileSync(filePath, JSON.stringify((subcribes), null,2));
-    res.status(200).json({status:"Message Recieved"});
-    console.log('Content from submited',{email});
-   } else {
-    //no file
-    subcribes.push(subcribe)
-    fs.writeFileSync(filePath, JSON.stringify((subcribes), null,2));
-    res.status(200).json({status:"Message Recieved"});
-    console.log('Content from submited',{email});
-   }
-
-   
+router.get('/', (req, res) => {
+    res.json(subject);
 });
 
-
 module.exports = router;
-
